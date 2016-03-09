456038b74c9bbdaed728a49cececa8b83d7cbe6e
stuntnmore <stuntnmore@gmail.com>
2016-03-09 03:51:19 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-# Linux FTP Backup Script
-# Version: 1.0
+# Linux MySQL Database FTP Backup Script
+# Version: 1.1
 # Script by: Pietro Marangon
 # Skype: pe46dro
 # Email: pietro.marangon@gmail.com
+# SFTP function by unixfox and Pe46dro
+# EDITED TO SUPPORT SQL Backup & Server Backup
+
+backup_path="/root"
+
+create_backup() {
+  umask 177
+
+  FILE="$db_name-$d.sql"
+  mysqldump --user=$user --password=$password --host=$host $db_name > /home/backup/$FILE
+
+  echo 'Backup Complete'
+}
 
 clean_backup() {
+  rm -f $backup_path/$FILE.sql
   rm -f ./$FILE
   echo 'Local Backup Removed'
 }
@@ -15,34 +29,41 @@ clean_backup() {
 # Edit Below This Line #
 ########################
 
+# Database credentials
+
+user="USER HERE"
+password="DATABASE PASS"
+host="localhost"
+db_name="DATABASE NAME"
+
 # FTP Login Data
-USERNAME="USERNAME HERE"
-PASSWORD="PASSWORD HERE"
-SERVER="IP HERE"
-PORT="REMOTE SERVER PORT"
+USERNAME="root"
+PASSWORD="SSH PASSWORD"
+SERVER="SERVER IP"
+PORT="22"
 
 #Directory where thing to backup is located
 DIR="/root"
 
 #Remote directory where the backup will be placed
-REMOTEDIR="./"
+REMOTEDIR="/home/backup"
 
-#Filename of backup file to be transfered DON'T WRITE EXTENSION (.tar/.zip/ecc...)
-FILE="BACKUP_NAME"
 
 #Transfer type
 #1=FTP
 #2=SFTP
-TYPE=1
+TYPE=2
 
 ##############################
 # Don't Edit Below This Line #
 ##############################
 
 d=$(date --iso)
+cd $backup_path
+create_backup
 
 FILE=$FILE"_"$d".tar.gz"
-tar -czvf ./$FILE $DIR
+tar -czvf ./$FILE $DIR #$DIR2
 echo 'Tar Complete'
 
 if [ $TYPE -eq 1 ]
@@ -50,12 +71,12 @@ then
 ftp -n -i $SERVER <<EOF
 user $USERNAME $PASSWORD
 binary
-put $FILE $REMOTEDIR/$FILE
+mput $FILE $REMOTDIR/$FILE
 quit
 EOF
 elif [ $TYPE -eq 2 ]
 then
-rsync --rsh="sshpass -p $PASSWORD ssh -p $PORT -o StrictHostKeyChecking=no -l $USERNAME" $FILE $SERVER:$REMOTEDIR
+rsync --rsh="sshpass -p $PASSWORD ssh -p $PORT -o StrictHostKeyChecking=no -l $USERNAME" $backup_path/$FILE $SERVER:$REMOTEDIR
 else
 echo 'Please select a valid type'
 fi
